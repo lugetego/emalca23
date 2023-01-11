aa9e10371c24bbc5f5765e4a74885e6e9d892b60
lugetego <lugetego123./>
2023-01-11 16:59:23 -0600

    Commit inicial

---
+++ src/Controller/RegistroController.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Registro;
+use App\Form\RegistroType;
+use App\Repository\RegistroRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/registro")
+ */
+class RegistroController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_registro_index", methods={"GET"})
+     */
+    public function index(RegistroRepository $registroRepository): Response
+    {
+        return $this->render('registro/index.html.twig', [
+            'registros' => $registroRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_registro_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, RegistroRepository $registroRepository,\Swift_Mailer $mailer): Response
+    {
+        $registro = new Registro();
+        $form = $this->createForm(RegistroType::class, $registro);
+        $form->remove('aceptado');
+        $form->remove('confirmado');
+        $form->remove('comentarios');
+        $form->remove('recomendacion');
+        $form->remove('cartaFile');
+
+        $twigglobals = $this->get("twig")->getGlobals();
+        $nombre_evento = $twigglobals["evento"];
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $registroRepository->add($registro, true);
+
+            // Mail
+            $message = (new \Swift_Message($nombre_evento.' - Registro'))
+                ->setFrom('webmaster@matmor.unam.mx')
+                ->setTo(array($registro->getMail() ))
+                //->setTo('gerardo@matmor.unam.mx')
+                ->setBcc(array('gerardo@matmor.unam.mx'))
+                ->setBody($this->renderView('mails/confirmacion.txt.twig', array('registro' => $registro)));
+
+            $mailer->send($message);
+
+            // Mail
+            $message = (new \Swift_Message($nombre_evento.' - Carta de recomendación'))
+                ->setFrom('webmaster@matmor.unam.mx')
+                ->setTo(array($registro->getMailprofesor() ))
+                //->setTo('gerardo@matmor.unam.mx')
+                ->setBcc(array('gerardo@matmor.unam.mx'))
+                ->setBody($this->renderView('mails/solcarta.txt.twig', array('registro' => $registro)));
+
+            $mailer->send($message);
+
+
+            //return $this->redirectToRoute('app_registro_index', [], Response::HTTP_SEE_OTHER);
+            return $this->render('registro/confirmacion.html.twig', [
+                'registro' => $registro,
+            ]);
+        }
+
+        return $this->renderForm('registro/new.html.twig', [
+            'registro' => $registro,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_registro_show", methods={"GET"})
+     */
+    public function show(Registro $registro): Response
+    {
+        return $this->render('registro/show.html.twig', [
+            'registro' => $registro,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_registro_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Registro $registro, RegistroRepository $registroRepository): Response
+    {
+        $form = $this->createForm(RegistroType::class, $registro);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $registroRepository->add($registro, true);
+
+            return $this->redirectToRoute('app_registro_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('registro/edit.html.twig', [
+            'registro' => $registro,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}/{mail}/recomendacion", name="app_registro_recomendacion", methods={"GET", "POST"})
+     */
+    public function recomendacion(Request $request, Registro $registro, RegistroRepository $registroRepository, $mail, $slug,\Swift_Mailer $mailer): Response
+    {
+        $form = $this->createForm(RegistroType::class, $registro);
+        $form->remove('nombre');
+        $form->remove('paterno');
+        $form->remove('materno');
+        $form->remove('sexo');
+        $form->remove('nacimiento');
+        $form->remove('mail');
+        $form->remove('procedencia');
+        $form->remove('carrera');
+        $form->remove('semestre');
+        $form->remove('porcentaje');
+        $form->remove('profesor');
+        $form->remove('promedio');
+        $form->remove('univprofesor');
+        $form->remove('mailprofesor');
+        $form->remove('confirmado');
+        $form->remove('vegetariano');
+        $form->remove('comentarios');
+        $form->remove('vegecomentario');
+        $form->remove('beca');
+        $form->remove('aceptado');
+        $form->remove('razones');
+        $twigglobals = $this->get("twig")->getGlobals();
+        $nombre_evento = $twigglobals["evento"];
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $registro->setModified(new \DateTime());
+
+            $registroRepository->add($registro, true);
+
+            // Mail
+            $message = (new \Swift_Message($nombre_evento.' - Carta de recomendación'))
+                ->setFrom('webmaster@matmor.unam.mx')
+                ->setTo(array($registro->getMailprofesor() ))
+                //->setTo('gerardo@matmor.unam.mx')
+                ->setBcc(array('gerardo@matmor.unam.mx'))
+                ->setBody($this->renderView('mails/confirmacion_carta.txt.twig', array('registro' => $registro)));
+
+            $mailer->send($message);
+
+//            return $this->redirectToRoute('app_registro_index', [], Response::HTTP_SEE_OTHER);
+            return $this->render('registro/confirmacion_carta.html.twig', [
+                'registro' => $registro,
+            ]);
+        }
+
+        if( $registro->getMail() == $registro->getMailprofesor() ||  $mail != $registro->getMail() || $slug != $registro->getSlug()){
+
+            throw $this->createNotFoundException('Existe algún problema con la información de registro favor de contactar a webmaster@matmor.unam.mx');
+        }
+
+        if( $registro->getCartaName() != null || $registro->getRecomendacion() != null)
+        {
+            return $this->render('registro/confirmacion_carta.html.twig', [
+                'registro' => $registro,
+            ]);
+        }
+
+
+        return $this->renderForm('registro/recomendacion.html.twig', [
+            'registro' => $registro,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_registro_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Registro $registro, RegistroRepository $registroRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$registro->getId(), $request->request->get('_token'))) {
+            $registroRepository->remove($registro, true);
+        }
+
+        return $this->redirectToRoute('app_registro_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+
+
+
+
+}
---
+++ src/Form/RegistroType.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Registro;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+
+class RegistroType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nombre')
+            ->add('paterno')
+            ->add('materno')
+            ->add('sexo', ChoiceType::class, [
+                'choices'  => [
+                    'Hombre' => 'Hombre',
+                    'Mujer' => 'Mujer',
+                ],
+                'placeholder' => 'Seleccionar',
+            ])
+            ->add('nacimiento',DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('mail')
+            ->add('procedencia')
+            ->add('carrera')
+            ->add('semestre')
+            ->add('porcentaje',ChoiceType::class, [
+                'choices'  => [
+                    '50'=>'50',
+                    '60'=>'60',
+                    '70'=>'70',
+                    '80'=>'80',
+                    '90'=>'90',
+                    '100'=>'100'
+                ],
+                'placeholder' => 'Seleccionar',
+            ])
+            ->add('profesor')
+            ->add('promedio',ChoiceType::class, [
+                'choices'  => [
+                    '8'=>'8.0',
+                    '8.1'=>'8.1',
+                    '8.2'=>'8.2',
+                    '8.3'=>'8.3',
+                    '8.4'=>'8.4',
+                    '8.5'=>'8.5',
+                    '8.6'=>'8.6',
+                    '8.7'=>'8.7',
+                    '8.8'=>'8.8',
+                    '8.9'=>'8.9',
+                    '9.0'=>'9.0',
+                    '9.1'=>'9.1',
+                    '9.2'=>'9.2',
+                    '9.3'=>'9.3',
+                    '9.4'=>'9.4',
+                    '9.5'=>'9.5',
+                    '9.6'=>'9.6',
+                    '9.7'=>'9.7',
+                    '9.8'=>'9.8',
+                    '9.9'=>'9.9',
+                    '10.0'=>'10.0'
+                ],
+                'placeholder' => 'Seleccionar',
+            ])
+            ->add('univprofesor')
+            ->add('mailprofesor')
+            ->add('confirmado')
+            ->add('vegetariano')
+            ->add('vegecomentario', TextareaType::class,array(
+                'required' => false,
+                'label'=>'Indicaciones'))
+
+            ->add('beca',ChoiceType::class, [
+                'choices'  => [
+                    'Solamente alimentación' => 'Solamente alimentación',
+                    'Solamente hospedaje' => 'Solamente hospedaje',
+                    'Hospedaje y alimentación' => 'Hospedaje y alimentación',
+                    'Transporte aéreo, sólo para estudiantes de América Latina y del Caribe (México no entra)'=> 'Transporte aéreo'
+                ],
+                'placeholder' => 'Seleccionar',
+            ])
+            ->add('aceptado')
+            ->add('razones', TextareaType::class,array(
+                'label'=>'Razones por las que desa asistir'))
+            ->add('comentarios', TextareaType::class,array(
+                'required' => false,
+                'label'=>'Comentarios'))
+            ->add('recomendacion', TextareaType::class,array(
+                'required' => false,
+                ))
+            ->add('cartaFile', VichFileType::class, [
+                'required' => false,
+                'label'=>'Carta de recomendación'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Registro::class,
+        ]);
+    }
+}
